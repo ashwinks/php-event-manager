dcffd71e499005dd1aaf6e1b4fe4c3adb9cc4234
Ashwin Surajbali <ashwin@redinkdesign.net>
2013-04-29 16:24:04 -0300

    initial commit

---
+++ EventManager.php
@@ -0,0 +1,144 @@
+<?php
+
+  class EventManager {
+	    
+	    public static $instance = null;
+	    
+	    private $_listeners = array();
+
+	    /**
+	     * 
+	     * @return Application_Model_Event_Manager
+	     */
+	    public static function getInstance(){
+	        
+	        if (!self::$instance){
+	            self::$instance = new self();
+	        }
+	        
+	        return self::$instance;
+	        
+	    }
+	    
+	    /**
+	     * 
+	     * @param string $event_name
+	     * @param array $data
+	     * @return Application_Model_Event_Manager
+	     */
+	    public function emit($event_name, array $data = array()){
+	        
+	        $listener = $this->getListener($event_name);
+	        if (!$listener){
+	            return $this;
+	        }
+	        
+	        foreach ($listener as $event){
+	            call_user_func($event['callback'], $data);
+	        }
+	        
+	        return $this;
+	        
+	    }
+	    
+	    /**
+	     * 
+	     * @param string $event_name
+	     * @param mixed $callback
+	     * @param int $priority
+	     * @return Application_Model_Event_Manager
+	     */
+	    public function on($event_name, $callback, $priority = 1){
+	          
+	        return $this->registerEvent($event_name, $callback, $priority);
+	        
+	    }
+	    
+	    /**
+	     * 
+	     * @param string $event_name
+	     * @return Application_Model_Event_Manager
+	     */
+	    public function detach($event_name){
+	        
+	        return $this->deRegisterEvent($event_name);
+	        
+	    }
+	    
+	    /**
+	     * 
+	     * @param string $event_name
+	     * @param mixed $callback
+	     * @param int $priority
+	     * @return Application_Model_Event_Manager
+	     */
+	    public final function registerEvent($event_name, $callback, $priority){
+	        
+	        $event_name = trim($event_name);
+
+        	if (!isset($this->_listeners[$event_name])){
+        		$this->_listeners[$event_name] = array();
+        	}
+
+        	$event = array(
+        		'event_name' => $event_name,
+        		'callback' => $callback,
+        		'priority' => (int) $priority
+        	);
+
+        	array_push($this->_listeners[$event_name], $event);
+        	
+        	if (count($this->_listeners[$event_name]) > 1){
+        		usort($this->_listeners[$event_name], array($this, '_sortListenerByPriority'));
+        	}
+
+        	return $this;
+
+	    }
+	    
+	    /**
+	     * 
+	     * @param string $event_name
+	     * @return Application_Model_Event_Manager
+	     */
+	    public final function deRegisterEvent($event_name){
+	        
+	        if (isset($this->_listeners[$event_name])){
+	            unset($this->_listeners[$event_name]);
+	        }
+	        
+	        return $this;
+	        
+	    }
+	    
+	    /**
+	     * 
+	     * @return multitype:array
+	     */
+	    public function getListeners(){
+	        
+	        return $this->_listeners;
+	        
+	    }
+	    
+	    public function getListener($listener){
+	        
+	        if (isset($this->_listeners[$listener])){
+	            return $this->_listeners[$listener];
+	        }
+	        
+	        return false;
+	        
+	    }
+	    
+	    private function _sortListenerByPriority($a, $b){
+	        
+	        if ($a['priority'] == $b['priority']){
+	            return 0;
+	        }
+	        
+	        return ($a['priority'] < $b['priority'])? -1 : 1;
+	        
+	    }
+	    
+	}
